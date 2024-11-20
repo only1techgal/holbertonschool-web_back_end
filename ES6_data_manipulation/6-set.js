9fa2e7de272d8d9ff6a3d0df8bd1badfbf3f0ac3
only1techgal <jenn.lipholo@gmail.com>
2024-11-20 12:58:17 +0200

    6. Set data structure

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Converts an array into a Set
+ * @param {Array} array - The array7 to be converted into a set
+ * returns {Set} A set constaining the unique values from the array
+ */
+export default function setFromArray(array) {
+    return new Set (array);
+}
