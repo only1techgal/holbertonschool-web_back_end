438a0dc4593036ea156739310034ab0a458a3e70
only1techgal <jenn.lipholo@gmail.com>
2024-11-20 11:26:19 +0200

    4. Combine

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+/**
+ * Updates students' grades based on their city and new grade data
+ * @param {Array} students - List of student objects
+ * @param {String} city - city to filter students by
+ * @param {Array} newGrades - Array of grade objects with studentId and grade
+ * @returns {Array} Array of updated student objects
+ */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+        const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
+        return {
+            ...students,
+            grade: gradeInfo ? gradeInfo.grade : 'N/A',
+        };
+    });  
+}
