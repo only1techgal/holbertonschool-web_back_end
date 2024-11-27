921d8ad5ead545ac3f61cd087e099ab037d9ea21
only1techgal <jenn.lipholo@gmail.com>
2024-11-27 14:47:57 +0200

    3. Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-/**
- * Handles profile signup by resolving promises for photo upload and user creation.
- * Logs the photo body, first name, and last name on success.
- * Logs an error message on failure.
- */
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
