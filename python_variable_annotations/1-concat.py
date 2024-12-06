df67156454951340e168970331ea8f4115deedc0
only1techgal <jenn.lipholo@gmail.com>
2024-12-06 15:30:18 +0200

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Module 1-concat.py
+This module contains a single function, `concat`,
+which takes a.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string.
+    """
+    return str1 + str2
