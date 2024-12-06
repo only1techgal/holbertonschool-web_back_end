3fee5952b3d9b0f22612a8bcb5fb49906e67c4f7
only1techgal <jenn.lipholo@gmail.com>
2024-12-06 16:26:43 +0200

    3. Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str.py
+This module contains a single function, `to_str`,
+which calculates the sum of two floating-point numbers.
+"""
+
+
+def to_str(n: float) -> float:
+    """
+    Convert a float to its string representation.
+
+    Parameters:
+    n(float): The float to convert.
+
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
