afbb683098abfc6313182fa50c245e622d7f433a
only1techgal <jenn.lipholo@gmail.com>
2024-12-06 16:52:19 +0200

    5. Complex types - list of floats mandatory

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculates the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): A list of floats.
+
+    Returns:
+        float: The sum of the floats in the list.
+    """
+    return sum(input_list)
